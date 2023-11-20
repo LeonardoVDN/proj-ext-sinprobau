f4fbc8c8b40f936d09558644f544b3cc5577f160
LeonardoVN <90997801+LeonardoVDN@users.noreply.github.com>
2023-11-20 15:09:13 -0300

    Back-end1
    Paginas contato e socio, ja estão adicionando informações no BD, Tela de Login iniciada e funcionando, Falta o painel administrativo e a resderização das noticias.

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+    $conn = mysqli_connect("localhost", "root", "");
+        mysqli_select_db($conn,"banco1");
+
+    $dsn = 'mysql:host=localhost;port=3306;dbname=banco1;';
+    $usuario = 'root';
+    $senha = ''; 
+
+?>
